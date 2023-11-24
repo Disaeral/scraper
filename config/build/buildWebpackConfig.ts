2bd992c3d2a940e24c48c6f23e6552a9f9a47f42
unknown <o1515584@gmail.com>
2023-11-25 00:55:01 +0900

    push of config folder

---
+++ config/build/buildWebpackConfig.ts
@@ -0,0 +1,32 @@
+import path from "path";
+import { BuildOptions } from "./types/config";
+import webpack from "webpack";
+import { buildPlugin } from "./buildPlugins";
+import { buildLoaders } from "./buildLoaders";
+import { buildResolvers } from "./buildResolvers";
+import { buildDevServer } from "./buildDevServer";
+
+
+
+export function buildWebpackConfig(options: BuildOptions):webpack.Configuration {
+    const {
+        paths, mode, isDev
+    } = options;
+    
+    return {
+        devtool: isDev ? 'inline-source-map' : undefined,
+        mode,
+        entry: paths.entry,
+        output: {
+            path: paths.build,
+            filename: '[name].[contenthash].js',
+            clean: true
+        },
+        plugins: buildPlugin(options),
+        module: {
+            rules: buildLoaders(options),
+        },
+        resolve: buildResolvers(),
+        devServer: isDev ? buildDevServer(options) : undefined,
+    }
+}
